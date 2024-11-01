d230a99ae690449df85db7b40c123f2be9b5bc86
nerdchild6 <posorun.posorungg@gmail.com>
2024-11-01 13:04:57 +0700

    initial

---
+++ project_server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/welcome", function(req, res) {
+    res.send("Welcome to you!");
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("Server is ready at " + port);
+});
